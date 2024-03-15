af2c2597239b29e67be47119d2b45dcd73266db2
Olesia Shcherbenko <oshch96@gmail.com>
2024-03-15 07:05:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import logo from './assets/logo.svg'
+//import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+    <img src={logo} alt="" />
+      <div className='app'>
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
